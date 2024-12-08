b2c1596332ec73b2b6b4f507761ab5ebd8245a95
BasiqNaeem <basiq.malik@gmail.com>
2024-12-08 20:15:31 +0500

    updated document-chat file

--- routes/document-chat.js
+++ routes/document-chat.js
@@ -2,7 +2,7 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import express, { json } from "express";
 const router = express.Router()
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { OpenAIEmbeddings,OpenAI } from "@langchain/openai";
+import { OpenAIEmbeddings, OpenAI } from "@langchain/openai";
 import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
 import { RetrievalQAChain } from "langchain/chains";
 import * as dotenv from "dotenv";
@@ -12,49 +12,46 @@ dotenv.config();
 
 
 
-router.get("/:question", async(req, res) => {
+router.get("/:question", async (req, res) => {
 
 
-const __dirname = import.meta.dirname;
+  const __dirname = import.meta.dirname;
 
-  const loader = new PDFLoader(join(__dirname, '.', 'practice_file.pdf' ));
-  
+  const loader = new PDFLoader(join(__dirname, '.', 'practice_file.pdf'));
+
   const docs = await loader.load();
 
-  
+
   const splitter = new RecursiveCharacterTextSplitter({
     chunkSize: 1000,
     chunkOverlap: 20,
   });
-  
+
 
   const splittedDocs = await splitter.splitDocuments(docs);
-  
+
+  // Note: The OpenAIEmbeddings class automatically reads the OPENAI_API_KEY from the environment variables have to set in env file.
   const embeddings = new OpenAIEmbeddings();
-  
-  const vectorStore = await HNSWLib.fromDocuments(  //in-memory vextor store
+
+  const vectorStore = await HNSWLib.fromDocuments(  //in-memory vector store
     splittedDocs,
     embeddings
   );
-  
+
   const vectorStoreRetriever = vectorStore.asRetriever();
-  
+
+  // Note: The OpenAI class automatically reads the OPENAI_API_KEY from the environment variables have to set in env file.
   const model = new OpenAI({
-    modelName: 'gpt-3.5-turbo',
+    modelName: 'gpt-3.5-turbo', // specify the model to use
     temperature: 0.5,
   });
-  
+
   const chain = RetrievalQAChain.fromLLM(model, vectorStoreRetriever);
-  
+
   const question = req.params.question;
-  
+
   const answer = await chain.call({
     query: question
-  });
-  
-  console.log({
-    question,
-    answer
   });
 
   res.json({
